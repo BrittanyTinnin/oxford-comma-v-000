76f5944b349e9f4216cd41ca63964da3f5054dd5
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-31 03:07:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   if array == array[0..1]
     return array.join(" and ")
   elsif array.length >= 3
-    array[-1]= "and " + array.last
+    # array[-1]= "and " + array.last
     #check if there are 3 or more item in the array
     #look at the last item in that array and change it to have an "and " before it
     #join each item in the array with a comma and a space and return that string
